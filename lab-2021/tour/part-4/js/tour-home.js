c2d82c9ba86bcf66cea39a229a04c910a158c318
chanj290 <70739058+chanj290@users.noreply.github.com>
2021-04-02 12:33:05 +0800

    hi

---
+++ lab-2021/tour/part-4/js/tour-home.js
@@ -0,0 +1,7 @@
+//var x = document.getElementById("myBtn");
+//x.addEventListener("mouseover", myFunction);
+//x.addEventListener("mouseout", myThirdFunction);
+
+//function myFunction() {
+//    document.getElementById("myBtn").innerHTML += "hi!<br>"
+ // }
