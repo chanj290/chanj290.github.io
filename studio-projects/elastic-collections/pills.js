e598c6593ffe9b97db1a421e85f9511844016fb2
chanj290 <70739058+chanj290@users.noreply.github.com>
2021-04-08 18:31:28 +0800

    hi

--- studio-projects/elastic-collections/pills.js
+++ studio-projects/elastic-collections/pills.js
@@ -61,48 +61,50 @@ function setTable(allItems) {
     itemImage.classList.add(item.fields.shape);
     container.appendChild(itemImage);
 
-  //     //Make the DIV element draggagle:
-  // dragElement(document.getElementById("____"));
-
-  // function dragElement(elmnt) {
-  //   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  //   if (document.getElementById(elmnt.id + "header")) {
-  //     /* if present, the header is where you move the DIV from:*/
-  //     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  //   } else {
-  //     /* otherwise, move the DIV from anywhere inside the DIV:*/
-  //     elmnt.onmousedown = dragMouseDown;
-  //   }
-
-  //   function dragMouseDown(e) {
-  //     e = e || window.event;
-  //     e.preventDefault();
-  //     // get the mouse cursor position at startup:
-  //     pos3 = e.clientX;
-  //     pos4 = e.clientY;
-  //     document.onmouseup = closeDragElement;
-  //     // call a function whenever the cursor moves:
-  //     document.onmousemove = elementDrag;
-  //   }
-
-  //   function elementDrag(e) {
-  //     e = e || window.event;
-  //     e.preventDefault();
-  //     // calculate the new cursor position:
-  //     pos1 = pos3 - e.clientX;
-  //     pos2 = pos4 - e.clientY;
-  //     pos3 = e.clientX;
-  //     pos4 = e.clientY;
-  //     // set the element's new position:
-  //     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-  //     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  //   }
-
-  //   function closeDragElement() {
-  //     /* stop moving when mouse button is released:*/
-  //     document.onmouseup = null;
-  //     document.onmousemove = null;
-  //   }
-  // }
   })
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// function getRandomPosition(element) {
+// 	var x = document.body.offsetHeight-element.clientHeight;
+// 	var y = document.body.offsetWidth-element.clientWidth;
+// 	var randomX = Math.floor(Math.random()*x);
+// 	var randomY = Math.floor(Math.random()*y);
+// 	return [randomX,randomY];
+// }
+// window.onload = function() {
+// 	var img = document.createElement('img');
+// 	img.setAttribute("style", "position:absolute;");
+// 	img.setAttribute("src", imageUrl);
+// 	document.body.appendChild(img);
+// 	var xy = getRandomPosition(img);
+// 	img.style.top = xy[0] + 'px';
+// 	img.style.left = xy[1] + 'px';
+// }
+
+
