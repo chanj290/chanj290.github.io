aff44d9a2a828c5eeb9632e27aeb4252a90da0bc
chanj290 <70739058+chanj290@users.noreply.github.com>
2021-04-13 23:55:49 +0800

    hi

--- studio-projects/elastic-collections/pills-final.js
+++ studio-projects/elastic-collections/pills-final.js
@@ -93,16 +93,6 @@ function dragItemFunction() {
   this.onmousedown = dragMouseDown
 }
 
-function dragMouseDown(e) {
-  e = e || window.event;
-  e.preventDefault();
-  // get the mouse cursor position at startup:
-  pos3 = e.clientX;
-  pos4 = e.clientY;
-  document.onmouseup = closeDragElement;
-  // call a function whenever the cursor moves:
-  document.onmousemove = elementDrag;
-}
 
 // --------- //
 
@@ -133,9 +123,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-// --------- //
-
-
-
-
